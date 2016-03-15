1c8d63178755395fe1c87ceadbd254acfed53395
Xerxous <Xerxous@github.com>
2016-03-14 23:00:38 -0400

    set static and template directories and created the home page app

--- cal/home/models.py
+++ cal/home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Veteran(models.Model):
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+	
+	
