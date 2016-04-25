c51df92dbe22439fe008c0d1cd9177d7647c625b
Xerxous <Xerxous@github.com>
2016-04-25 13:34:44 -0400

    template organization update

--- cal/apps/home/views.py
+++ cal/apps/home/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import logout
 # Create your views here.
 def landing(request):
     if request.user.is_authenticated():
-        return render(request, 'home/index.html')
+        return render(request, 'home/index.html', { 'group':str(request.user.groups.all()[0].name) })
     return redirect('login:login')
 
 def sign_out(request):
