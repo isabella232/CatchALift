6f9345a47b4891a552ee78cfc3c5785420c169e7
Xerxous <Xerxous@github.com>
2016-04-26 16:40:45 -0400

    account manager added

--- cal/apps/home/url.py
+++ cal/apps/home/url.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.landing, name='index'),
+    url(r'^manager$', views.landing, name='manager'),
     url(r'^sign_out$', views.sign_out, name='sign_out'),
 ]
--- cal/apps/home/views.py
+++ cal/apps/home/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from django.contrib.auth.models import User
 
 def landing(request):
-    if request.user.is_authenticated():
-        return render(request, 'home/index.html', {'group': str(request.user.groups.all()[0].name)})
-    return redirect('login:login')
+    if not request.user.is_authenticated():
+        return redirect('login:login')
+    context = dict()
+    context['name'] = str(request.user.get_username())
+    context['group'] = str(request.user.groups.all()[0].name)
+    context['page'] = str(request.path)
+
+    if request.path == '/manager':
+        context['coach_list'] = User.objects.filter(groups__name='Coach')
+        context['user_list'] = User.objects.filter(groups__name='User')
+
+    return render(request, 'home/index.html', context)
 
 def sign_out(request):
     logout(request)
